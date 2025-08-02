c9a52357e0c9558e20ce143b9152bd1d1a9e8ac3
eipiriru <ainurkhaprianto@gmail.com>
2025-08-02 09:54:20 +0700

    basic web - template

--- webapps/web1.go
+++ webapps/web1.go
@@ -2,15 +2,19 @@ package webapps
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"path"
 )
 
+var rootProj string = "webapps"
+
 func Route(address string) {
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(path.Join(rootProj, "assets")))))
 	http.HandleFunc("/", index)
 	http.HandleFunc("/hello", hello)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("webapps/assets"))))
 
-	fmt.Println(http.Dir("/webapps/assets"))
 	fmt.Println("Server started on", address)
 
 	server := new(http.Server)
@@ -23,8 +27,23 @@ func Route(address string) {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	msg := "Hello ini Index"
-	w.Write([]byte(msg))
+	var filepath = path.Join(rootProj, "views", "index.html")
+	var tmp, err = template.ParseFiles(filepath)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var data = map[string]interface{}{
+		"title": "Apa 1",
+		"name":  "Hello World Building",
+	}
+
+	err = tmp.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
